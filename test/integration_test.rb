bdb41936f15d3a62dc86323018412d96436a5935
Yves Senn <yves.senn@gmail.com>
2016-10-25 13:13:27 +0200

    test for error logging.

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -7,7 +7,11 @@ class IntegrationTest < SidekiqIchniteTestCase
     include Sidekiq::Worker
 
     def perform(var)
-      Ichnite.log('job_run', var: var)
+      if var == 'fail'
+        raise ArgumentError, 'oh no'
+      else
+        Ichnite.log('job_run', var: var)
+      end
     end
   end
 
@@ -19,7 +23,7 @@ class IntegrationTest < SidekiqIchniteTestCase
     assert_match(/event=job_enqueue queue=default job_id=[a-f0-9]+ job_class=IntegrationTest::MyWorker args=\["some data"\]/, line)
   end
 
-  def test_server_log
+  def test_server_log_success
     worker = MyWorker.new
     Sidekiq::Ichnite::ServerMiddleware.new.call(worker, SIDEKIQ_WORKER_MSG, 'default')  do
       worker.perform('some data')
@@ -28,5 +32,18 @@ class IntegrationTest < SidekiqIchniteTestCase
     assert_ichnite_logs(
       "event=job_run job_id=0b59872d5a115e833b4232d6 job_class=SampleWorker var=some data",
       "event=job_stop job_id=0b59872d5a115e833b4232d6 job_class=SampleWorker queue=default duration=0")
+  end
+
+  def test_server_log_error
+    worker = MyWorker.new
+    begin
+      Sidekiq::Ichnite::ServerMiddleware.new.call(worker, SIDEKIQ_WORKER_MSG, 'default')  do
+        worker.perform('fail')
+      end
+    rescue ArgumentError
+    end
+
+    assert_ichnite_logs(
+      'event=job_error job_id=0b59872d5a115e833b4232d6 job_class=SampleWorker queue=default at=error duration=0 error=\'ArgumentError\' message="oh no"')
   end
 end
